b0cbc3ec500a761d7c5ec0b8f4ad87e1b14ad18e
Bubba Godinet <bubba@MasterPC>
2024-04-24 16:25:40 -0600

    all stuff befor twin

---
+++ src/app/components/mask-cursor/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const MaskCursor = () => {
+return (
+<div className='bg-purple fw:bg-tahiti'>
+ herro world
+</div>
+)
+}
+export default MaskCursor
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,35 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: '#ffffff',
+      purple: '#3f3cbb',
+      midnight: '#121063',
+      metal: '#565584',
+      tahiti: '#3ab7bf',
+      silver: '#ecebff',
+      bubblegum: '#ff77e9',
+      bermuda: '#78dcca',
+    },
+    screens: {
+      mob: { 'max': '428px' },
+      tab: { 'min': '429px', 'max': '1024px' },
+      dt: { 'min': '1025px', 'max': '1440px' },
+      fw: '1441px',
+    },
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
